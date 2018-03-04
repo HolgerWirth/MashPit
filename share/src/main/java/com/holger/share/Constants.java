716dec338e51d5ca739ea3547d2807a044598ae7
Holger <holgi.wirth@gmail.com>
2018-03-04 11:18:03 +0100

    New shared library for constants

--- share/src/main/java/com/holger/share/Constants.java
+++ share/src/main/java/com/holger/share/Constants.java
@@ -1,7 +1,7 @@
-package com.holger.mashpit;
+package com.holger.share;
 
-class Constants {
-    interface ACTION {
+public class Constants {
+    public interface ACTION {
         String MAIN_ACTION = "com.holger.mashpit.action.main";
         String CANCEL_ACTION = "com.holger.mashpit.action.cancel";
         String STARTFOREGROUND_ACTION = "com.holger.mashpit.action.startforeground";
@@ -9,13 +9,20 @@ class Constants {
         String CONNECT_ACTION = "com.holger.mashpit.action.connect";
         String RECONNECT_ACTION = "com.holger.mashpit.action.reconnect";
         String CHECK_ACTION = "com.holger.mashpit.action.check";
+
     }
 
-    interface PREFS {
+   public interface WEAR {
+        String RUN_UPDATE_NOTIFICATION = "/MashPit";
+        String KEY_TITLE = "/title";
+        String KEY_CONTENT = "/content";
+    }
+
+    public interface PREFS {
         String PREFS_KEY_FIRST_START = "first_start";
     }
 
-    interface NOTIFICATION_ID {
+    public interface NOTIFICATION_ID {
         int FOREGROUND_SERVICE = 101;
     }
 }
