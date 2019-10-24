24754574d0f4aa310993a39a66ae2e07d1e7827d
HolgerWirth <holgi.wirth@gmail.com>
2019-10-24 16:30:47 +0200

    Fixed the service reconnect

--- app/src/main/java/com/holger/mashpit/prefs/MQTTSettingsSub.java
+++ app/src/main/java/com/holger/mashpit/prefs/MQTTSettingsSub.java
@@ -1,5 +1,6 @@
 package com.holger.mashpit.prefs;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -12,22 +13,50 @@ import androidx.appcompat.app.AlertDialog;
 import com.holger.mashpit.MashPit;
 import com.holger.mashpit.R;
 import com.holger.mashpit.TemperatureService;
+import com.holger.share.Constants;
 
 public class MQTTSettingsSub extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     private static final String DEBUG_TAG = "MQTTSettingsPub";
+    private boolean changed=false;
+    Context context;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.prefs_mqtt_sub);
+        context=getPreferenceScreen().getContext();
 
         MashPit.reconnect_action = false;
+    }
 
+    @Override
+    public void onDestroy() {
+        Log.i(DEBUG_TAG, "onDestroy()");
+        super.onDestroy();
     }
 
     @Override
     public void onStop() {
+        Log.i(DEBUG_TAG, "onStop");
+        if (changed) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(context);
+            builder.setTitle(getString(R.string.MQTTchanged_alert_title));
+            builder.setMessage(getString(R.string.MQTTchanged_text));
+            builder.setPositiveButton(getString(R.string.MQTTchanged_button), new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Log.i(DEBUG_TAG, "Reconnect pressed!");
+                    MashPit.reconnect_action = true;
+                        Log.i(DEBUG_TAG, "Reconnect service!");
+                        Intent startIntent = new Intent(context, TemperatureService.class);
+                        startIntent.setAction(Constants.ACTION.RECONNECT_ACTION);
+                        context.startService(startIntent);
+                }
+            });
+            builder.setNegativeButton(getString(R.string.MQTTchanged_cancel), null);
+            builder.show();
+        }
         super.onStop();
         getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
     }
@@ -41,7 +70,6 @@ public class MQTTSettingsSub extends PreferenceFragment implements SharedPrefere
     @Override
     public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
         Log.i(DEBUG_TAG, "Key: " + key);
-        boolean changed = false;
 
         if (key.equals("broker_url")) {
             changed = true;
@@ -68,23 +96,6 @@ public class MQTTSettingsSub extends PreferenceFragment implements SharedPrefere
             editor.putString("send_broker_user", prefs.getString("broker_user", ""));
             editor.putString("send_broker_password", prefs.getString("broker_password", ""));
             editor.apply();
-        }
-
-        if (changed) {
-            AlertDialog.Builder builder = new AlertDialog.Builder(getPreferenceScreen().getContext());
-            builder.setTitle(getString(R.string.MQTTchanged_alert_title));
-            builder.setMessage(getString(R.string.MQTTchanged_text));
-            builder.setPositiveButton(getString(R.string.MQTTchanged_button), new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    Log.i(DEBUG_TAG, "Reconnect pressed!");
-                    MashPit.reconnect_action = true;
-//                    getActivity().stopService(new Intent(getActivity(), TemperatureService.class));
-//                    getActivity().startService(new Intent(getActivity(), TemperatureService.class));
-                }
-            });
-            builder.setNegativeButton(getString(R.string.MQTTchanged_cancel), null);
-            builder.show();
         }
     }
 }
