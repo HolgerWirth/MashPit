8de87460bca34d52827de4982439d17b0b44701b
Holger <holgi.wirth@gmail.com>
2020-05-19 11:08:06 +0200

    New methods for retrieving alias name for server and sensor.

--- app/src/main/java/com/holger/mashpit/tools/SubscriptionHandler.java
+++ app/src/main/java/com/holger/mashpit/tools/SubscriptionHandler.java
@@ -4,6 +4,8 @@ import android.database.Cursor;
 
 import com.activeandroid.Cache;
 import com.activeandroid.query.Select;
+import com.holger.mashpit.model.SensorStatus;
+import com.holger.mashpit.model.Sensors;
 import com.holger.mashpit.model.Subscriptions;
 
 import java.util.ArrayList;
@@ -11,7 +13,6 @@ import java.util.List;
 
 public class SubscriptionHandler {
     private List<String> subscriptions;
-    private List<String> subscriptions_durable;
     private String action;
 
     public SubscriptionHandler(String action)
@@ -23,17 +24,28 @@ public class SubscriptionHandler {
     public void refreshSubscription()
     {
         subscriptions = new ArrayList<>();
-        subscriptions_durable = new ArrayList<>();
         List<Subscriptions> dbresult = new Select().from(Subscriptions.class).where("action=?",action).orderBy("server ASC").execute();
         for (Subscriptions sub : dbresult) {
             subscriptions.add(sub.server+"/"+sub.sensor+"/"+sub.interval);
-            if(sub.durable==1)
-            {
-                subscriptions.add(sub.server+"/"+sub.sensor+"/"+sub.interval);
-            }
         }
     }
 
+    public String getServerAlias(String server) {
+        SensorStatus sensorStatus = new Select().from(SensorStatus.class).where("server=?", server).executeSingle();
+        if (!sensorStatus.alias.isEmpty()) {
+            return sensorStatus.alias;
+        }
+        return sensorStatus.server;
+    }
+
+    public String getSensorAlias(String server,String sensor) {
+        Sensors sensors = new Select().from(Sensors.class).where("server = ?", server).and("sensor = ?",sensor).executeSingle();
+        if(sensors.name.isEmpty())
+        {
+            return sensors.sensor;
+        }
+        return sensors.name;
+    }
     public List<String> getAllSubscription(boolean durable) {
         List<String> sub_all = new ArrayList<>();
         final String sql="select server,sensor,interval, sum(durable) from Subscriptions group by server,sensor,interval";
