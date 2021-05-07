4464958149c7d8e3d8f4220d8e2af1c78eebba32
HolgerWirth <holgi.wirth@gmail.com>
2021-05-07 11:40:14 +0200

    Changed persistent layer from ActiveAndroid to ObjectBox

--- app/src/main/java/com/holger/mashpit/model/Subscriptions.java
+++ app/src/main/java/com/holger/mashpit/model/Subscriptions.java
@@ -1,46 +1,24 @@
 package com.holger.mashpit.model;
 
-import com.activeandroid.Model;
-import com.activeandroid.annotation.Column;
-import com.activeandroid.annotation.Table;
+import io.objectbox.annotation.Entity;
+import io.objectbox.annotation.Id;
+import io.objectbox.annotation.Transient;
 
-import java.io.Serializable;
+@Entity
+    public class Subscriptions {
+    @Id
+    public long id;
 
-@Table(name = "Subscriptions")
-public class Subscriptions extends Model implements Serializable {
-    @Column(name = "topic", index = true)
     public String topic;
-    @Column(name="action", index = true)
     public String action;
-    @Column(name="name", index = true)
     public String name;
-    @Column(name = "server", index = true)
     public String server;
-    @Column(name = "sensor", index = true)
     public String sensor;
-    @Column(name = "interval")
     public int interval;
-    @Column(name = "durable")
     public int durable;
-    @Column(name = "deleted")
     public boolean deleted;
-
-    public String aliasServer;
+    @Transient
     public String aliasSensor;
-
-    public Subscriptions() {
-        super();
-    }
-
-    public Subscriptions(String topic, String action,String name,String server, String sensor, int interval, int durable, boolean deleted)
-    {
-        this.topic = topic;
-        this.action = action;
-        this.name = name;
-        this.server = server;
-        this.sensor = sensor;
-        this.interval = interval;
-        this.durable = durable;
-        this.deleted = deleted;
-    }
+    @Transient
+    public String aliasServer;
 }
